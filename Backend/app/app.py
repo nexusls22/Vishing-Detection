dd94bffd0a648cea65fe81a9aa49fba26ddb6aa7
DESKTOP-ID09F56\Luis <nexusls22@gmail.com>
2026-02-24 17:36:30 +0000

    Seperating the Backend folder, encapsulation of data_manager

---
+++ Backend/app/app.py
@@ -0,0 +1,26 @@
+import os.path
+from itables import init_notebook_mode, show
+
+from Backend.data_manager.DataManager import DataManager
+
+init_notebook_mode(all_interactive=True)
+data_folder_path = os.path.abspath(os.sep) + 'Users/Luis/Desktop/LA/LA/'
+results = []
+
+def main():
+    data_manager = DataManager(data_folder_path)
+    get_dir_info(data_manager)
+    get_files_list(data_manager)
+
+
+def get_i_table():
+    data_manager = DataManager(data_folder_path)
+    df = data_manager.load_data(results, data_manager.input_sample_size)
+
+    return df
+
+def get_dir_info(data_manager):
+    return data_manager.check_data_dir()
+
+def get_files_list(data_manager):
+    return data_manager.check_data_files()
