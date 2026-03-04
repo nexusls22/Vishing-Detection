4d6a5f8e0fccc3ae13ffddf8c83a54b1da05faa0
DESKTOP-ID09F56\Luis <nexusls22@gmail.com>
2026-03-04 15:34:47 +0000

    Changes to DataManager and comments for future tasks

--- Backend/app.py
+++ Backend/app.py
@@ -1,13 +1,24 @@
 import os.path
 from itables import show
 
-from Backend.data.datamanager import DataManager
+from Backend.data.datamanager import DataManager, check_directory, check_data_quality
 
 data_folder_path = os.path.abspath(os.sep) + 'Users/Luis/Desktop/LA/LA/'
 input_sample_size = 300
 data_manager = DataManager(data_folder_path, 'train', input_sample_size)
 results = []
 
+## For next step: Proper classes for regression, learning etc
+#Backend/
+#├── data/
+#│   └── datamanager.py     # DataManager (Daten laden/validieren)
+#├── network/
+#│   ├── __init__.py
+#│   ├── models.py          # Neural Network Definition
+#│   ├── regression.py      # Sklearn-Models
+#│   ├── trainer.py         # Train logic with epochs
+#│   └── utils.py           # Helper functions
+#└── app.py                 # Streamlit UI (only Presentation)
 
 def main():
 
@@ -23,7 +34,7 @@ def get_data():
     return data_manager.load_data()
 
 def get_dir_info():
-    return data_manager.check_directory(data_folder_path)
+    return check_directory(data_folder_path)
 
 def check_data_files(df):
-    return data_manager.check_data_quality(df)
+    return check_data_quality(df)
